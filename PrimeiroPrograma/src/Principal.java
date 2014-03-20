6c23d7c9e2d68238b59efa50534b17de149a56e2
Mauriciobertodo <mauriciobertodo@gmail.com>
2014-03-20 18:45:08 -0300

    PrimeiroPrograma versão 3

--- PrimeiroPrograma/src/Principal.java
+++ PrimeiroPrograma/src/Principal.java
@@ -3,7 +3,7 @@ import javax.swing.JOptionPane;
 public class Principal {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "HELLO WORD!!!!!!!! \n\n\n Versão 2");
+		JOptionPane.showMessageDialog(null, "HELLO WORD!!!!!!!! \n\n\n Versao 3");
 	}
 
 }
