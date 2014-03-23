872a85a10bae9c08ea14ea66c69996b9c87f5e6c
Mauriciobertodo <mauriciobertodo@gmail.com>
2014-03-23 20:20:32 -0300

    Lista Encadeada versão 3
    Arquivo de contatos incluido

--- Trabalho1_ListaEncadeada/src/com/senac/apps/Controller.java
+++ Trabalho1_ListaEncadeada/src/com/senac/apps/Controller.java
@@ -1,19 +1,22 @@
 package com.senac.apps;
 
 import com.senac.apps.views.ConsoleView;
+import com.senac.arquivo.LerArquivo;
 import com.senac.estruturas.ListaOrdenada;
 import com.senac.estruturas.Nodo;
+import com.senac.mvc.controller.PessoaController;
+import com.senac.mvc.model.Pessoa;
+import com.senac.mvc.view.ContatoView;
 
 public class Controller {
 	
-	private ListaOrdenada<Integer>
+	private ListaOrdenada<String>
 			
-	lista = new ListaOrdenada<Integer>();
+	lista = new ListaOrdenada<String>();
 	
 	private ConsoleView view = new ConsoleView();
 	
 	public void iniciaLista() {
-		
 		
 		/*
 		for (int i = 0; i < 50; ++i) {
@@ -21,14 +24,13 @@ public class Controller {
 			novo.setChave((int)(Math.random() * 10000));
 			lista.insert(novo);
 		}
-		*/
-		
+		*/		
 	}
 	
 	public void imprimeLista() {
-		Nodo<Integer> nodo = lista.getHead();
+		Nodo<String> nodo = lista.getHead();
 		while (nodo != null) {
-			view.imprimeInteiro(nodo.getChave());
+			//view.imprimeInteiro(nodo.getChave());
 			nodo = nodo.getNext();
 		}
 	}	
--- Trabalho1_ListaEncadeada/src/com/senac/mvc/controller/PessoaController.java
+++ Trabalho1_ListaEncadeada/src/com/senac/mvc/controller/PessoaController.java
@@ -1,5 +1,4 @@
 package com.senac.mvc.controller;
-
 import com.senac.mvc.model.Pessoa;
 import com.senac.mvc.view.ContatoView;
 
@@ -7,14 +6,10 @@ public class PessoaController {
 
 	public Pessoa contato;
 	
-	public void criarContato()
-	{
-		contato = new Pessoa();
-	}
-	
-	public void exibirContato( ContatoView view )
-	{
+	public void criarContato(){
+		contato = new Pessoa();	
+	}	
+	public void exibirContato( ContatoView view ){
 		//view.printContato(contato.getNome(), contato.getTelefone());
-	}
-	
+	}	
 }
