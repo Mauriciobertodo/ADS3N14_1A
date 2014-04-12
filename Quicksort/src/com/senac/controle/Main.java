8e4c981ec554e92f428094d715b003198e422f9e
Mauriciobertodo <mauriciobertodo@gmail.com>
2014-04-11 21:15:21 -0300

    Quicksort versao 1

---
+++ Quicksort/src/com/senac/controle/Main.java
@@ -0,0 +1,11 @@
+package com.senac.controle;
+
+import com.senac.principal.Quicksort;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Quicksort inicia = new Quicksort();
+		inicia.Ordenar();
+	}
+}
---
+++ Quicksort/src/com/senac/principal/Quicksort.java
@@ -0,0 +1,8 @@
+package com.senac.principal;
+
+public class Quicksort {
+	
+	public void Ordenar() {
+		
+	}
+}
