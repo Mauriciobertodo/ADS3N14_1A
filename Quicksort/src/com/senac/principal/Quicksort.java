04b017ac947d8e7b3ad03d847b3dc5538dafdda1
Mauriciobertodo <mauriciobertodo@gmail.com>
2014-04-11 21:17:10 -0300

    Quicksort versao 2
    Gera numeros desordenados

--- Quicksort/src/com/senac/principal/Quicksort.java
+++ Quicksort/src/com/senac/principal/Quicksort.java
@@ -1,8 +1,22 @@
 package com.senac.principal;
 
+import java.util.Random;
+
 public class Quicksort {
 	
 	public void Ordenar() {
+		Random rand = new Random();
 		
+		int quant = 10;
+		int[] vet = new int[quant];
+		int aux = 0;
+		
+		for(int i = 0; i < quant; i++){
+			vet[i] = rand.nextInt(10);
+		}		
+		System.out.println("Desordenado: ");
+		for (int i = 0; i < quant; i++) {
+			System.out.print(vet[i] + " ");
+		}		
 	}
 }
