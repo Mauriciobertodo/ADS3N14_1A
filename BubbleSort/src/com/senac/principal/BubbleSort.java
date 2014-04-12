b97ccdce89bda232d09c1b3b5e97f7a96f3b986d
Mauriciobertodo <mauriciobertodo@gmail.com>
2014-04-11 21:10:53 -0300

    BubbleSort versao 3
    Gera numeros e mostra ordenado

--- BubbleSort/src/com/senac/principal/BubbleSort.java
+++ BubbleSort/src/com/senac/principal/BubbleSort.java
@@ -9,12 +9,26 @@ public class BubbleSort {
 		
 		int quant = 10;
 		int[] vet = new int[quant];
-						
-		for(int i = 0; i < quant; i++)
-		{
+		int aux = 0;
+		
+		for(int i = 0; i < quant; i++){
 			vet[i] = rand.nextInt(10);
 		}		
 		System.out.println("Desordenado: ");
+		for (int i = 0; i < quant; i++) {
+			System.out.print(vet[i] + " ");
+		}
+		System.out.println(" ");
+		for (int i = 0; i < quant; i++) {
+			for (int j = 0; j < quant - 1; j++) {
+				if (vet[j] > vet[j + 1]) {
+					aux = vet[j];
+					vet[j] = vet[j + 1];
+					vet[j + 1] = aux;
+				}
+			}
+		}
+		System.out.println("\nOrganizado:");
 		for (int i = 0; i < quant; i++) {
 			System.out.print(vet[i] + " ");
 		}
